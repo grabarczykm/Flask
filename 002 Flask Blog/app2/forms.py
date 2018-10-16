73004da072c0d41ec389474cb6ee7ccec7299ee9
AlmightyFist <marcingrabarczyk93@gmail.com>
2018-10-16 16:26:06 +0200

    New post.html, updating account information

--- 002 Flask Blog/app2/forms.py
+++ 002 Flask Blog/app2/forms.py
@@ -1,8 +1,10 @@
 """ Place for forms """
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField #importowanie klas pól do formualrza
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField #importowanie klas pól do formualrza
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError #importowanie klas validatorów ||
 from app2.models import User
+from flask_login import current_user
 # DataRequired - pole nie moze zostać puste
 
 #Formularz rejestracji
@@ -36,3 +38,31 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login') #pole przesyłające dane z całego formularza
+
+# Aktualizacja konta uzytkownika
+class UpdateAccountForm(FlaskForm):
+
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min = 2, max=20)]) #pierwszy argument to wyświetlana etykieta, drugi to ograniczenia nałożone na wprowadzoną wartosć
+    email = StringField('Email',validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update') #pole przesyłające dane z całego formularza
+
+    #Funkcja służąca formularza aktualizacji
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username = username.data).first() # sprawdzanie czy istnieje użytkownik o username wprowadzonym wlasnie do formularza rejestracji
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()  # sprawdzanie czy istnieje użytkownik o username wprowadzonym wlasnie do formularza rejestracji
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
+
+#Tworzenie posta
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired])
+    submit = SubmitField('Post')
--- 002 Flask Blog/app2/routes.py
+++ 002 Flask Blog/app2/routes.py
@@ -1,5 +1,7 @@
+import os
+import secrets
 from flask import render_template, url_for, flash, redirect, request
-from app2.forms import RegistrationForm, LoginForm
+from app2.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from app2 import app, db, bcrypt
 from app2.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
@@ -78,7 +80,54 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route("/account")
+#Zapisywanie obrazka przesłanego w formularzu do bazy danych
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename) #robicie nazwy załadowanego pliku na nazwę i rozszerzenie, tak aby zapisać obraz w tym samych formacie
+    picture_fn = random_hex + f_ext #utworzenie nowej nazwy obrazka na podstawie randomowego tokena i rozszerzenia pliku
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn) #ścieżka do zapisania nowego pliku
+
+    #Zmiana rozmiaru zdjęcia tutaj zamiast w CSS żeby zaoszczędzić czas i moc na przesyłaniu zdjęć dużej wielkości
+    output_size = (125,125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+
+    i.save(picture_path) #zapisanie załadowanego obrazka
+
+    return picture_fn
+
+
+@app.route("/account", methods=['POST', 'GET'])
 @login_required #dekorator stawiający wymaganie zalogowania się - jeżeli spróbujemy wejsć na ten adres bez logowania, przekieruje do widoku logowania
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+
+    #Jeżeli formularz jest przesłany i prawidłowy, zaktualizowanie danych użytkownika
+    if form.validate_on_submit():
+        if form.picture.data: #jeżeli w formularzu przesyłany jest nowy obrazek
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!','success')
+        return redirect(url_for('account'))
+
+    #Jeżeli formularz nie jest przesłany, pola wypełnione są aktualnymi danymi
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account',
+                           image_file=image_file, form=form)
+
+@app.route("/post/new", methods=['POST', 'GET'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+
+    return render_template('create_post.html', title='New Post', form=form)
